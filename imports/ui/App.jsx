89754ad95a652e1b4105803ff87a02845bb221da
ishwiz <154478861+ishwiz@users.noreply.github.com>
2025-04-09 00:34:44 +1000

    render sample collection

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import { Task } from './Task';
+import React from "react";
+import { useTracker, useSubscribe } from "meteor/react-meteor-data";
+import { TasksCollection } from "/imports/api/TasksCollection";
+import { Task } from "./Task";
 
-const tasks = [
-    {_id: 1, text: 'Water plants'},
-    {_id: 2, text: 'Buy Cat Food'},
-    {_id: 3, text: 'Chill with my cat named Nova'}
-];
+export const App = () => {
+  const isLoading = useSubscribe("tasks"); 
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor</h1>
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
 
-    <ul>
-    { tasks.map(task => <Task key={ task._id } task={ task }/>) }
-    </ul>
-  </div>
-)
+      <ul>
+        {tasks.map((task) => (
+          <Task key={task._id} task={task} />
+        ))}
+      </ul>
+    </div>
+  );
+};
